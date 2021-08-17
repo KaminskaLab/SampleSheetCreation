35d7057f9060882e0401a9e89c80af92aab4be14
KaminskaLab <88099740+KaminskaLab@users.noreply.github.com>
2021-08-17 18:14:32 +0200

    Update chromium10X_indexes.R

--- chromium10X_indexes.R
+++ chromium10X_indexes.R
@@ -2,8 +2,9 @@
 
 setwd(".")
 
-sheet <- read.csv("lowerpart.csv", header = FALSE)
-idxRef <- read.csv("chromium_sample_indexes.csv", header = FALSE)
+sheet <- read.csv("lowerpart.csv", header = FALSE, sep=",")
+idxRef <- read.csv("chromium_sample_indexes.csv", header = FALSE, sep = " ")
+idxRef <- idxRef[ , colSums(is.na(idxRef)) == 0]
 idx <- read.csv("tmp.txt", header = FALSE)
 
 nr <- lapply(1:dim(idx)[1],function(x){grep(idx[x,1],idxRef[,grep(idx[x,1],idxRef)])})
@@ -12,4 +13,4 @@ indexes_10x <- lapply(nr, function(x){return(idxRef[x,1])})
 
 sheet[,6] <- unlist(indexes_10x)
 
-write.table(x = sheet, file = "lower_new.csv",  row.names = FALSE, col.names = FALSE)
+write.table(x = sheet, file = "lower_new.csv", col.names = FALSE, na = "", sep=",")
