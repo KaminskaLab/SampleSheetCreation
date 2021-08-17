bf038a57c51ef7947f1ca647ca5d63e9a12b1331
KaminskaLab <88099740+KaminskaLab@users.noreply.github.com>
2021-08-17 16:06:11 +0200

    Create chromium_indexes.R

---
+++ chromium_indexes.R
@@ -0,0 +1,15 @@
+# it is called in a bash script with the same name
+
+setwd(".")
+
+sheet <- read.csv("lowerpart.csv", header = FALSE)
+idxRef <- read.csv("chromium_sample_indexes.csv", header = FALSE)
+idx <- read.csv("tmp.txt", header = FALSE)
+
+nr <- lapply(1:dim(idx)[1],function(x){grep(idx[x,1],idxRef[,grep(idx[x,1],idxRef)])})
+
+indexes_10x <- lapply(nr, function(x){return(idxRef[x,1])})
+
+sheet[,6] <- unlist(indexes_10x)
+
+write.table(x = sheet, file = "lower_new.csv",  row.names = FALSE, col.names = FALSE)
